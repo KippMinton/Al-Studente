54ae3e1c257f6b4919c4ce961bc6e0cfb78915d8
Kipp Minton <luciankippminton@gmail.com>
2021-10-20 13:45:37 -0500

    first commit; firebase authentication

---
+++ FirebaseMVC/Auth/IFirebaseAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using AlStudente.Auth.Models;
+
+namespace AlStudente.Auth
+{
+    public interface IFirebaseAuthService
+    {
+        Task<FirebaseUser> Login(Credentials credentials);
+        Task<FirebaseUser> Register(Registration registration);
+    }
+}
---
+++ FirebaseMVC/Auth/Models/Credentials.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AlStudente.Auth.Models
+{
+    public class Credentials
+    {
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
